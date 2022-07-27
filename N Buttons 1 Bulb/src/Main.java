92e6033b95df1e2da4cd271208ef4623f209897f
divyesh006 <divyesh10008@gmail.com>
2022-07-27 22:04:47 +0530

    Codechef

--- N Buttons 1 Bulb/src/Main.java
+++ N Buttons 1 Bulb/src/Main.java
@@ -6,9 +6,19 @@ public class Main {
         int t = sc.nextInt();
         while (t-- > 0){
             int n = sc.nextInt();
-            String a = new String(String.valueOf(n));
-            a = sc.nextLine();
-            
+            String s1 = sc.next();
+            String s2 = sc.next();
+            int count = 0;
+            for (int i = 0;i < n;i++){
+                if (s1.charAt(i) != s2.charAt(i)){
+                    count++;
+                }
+            }
+            if (count % 2 == 0){
+                System.out.println("1");
+            }else {
+                System.out.println("0");
+            }
         }
     }
 }
