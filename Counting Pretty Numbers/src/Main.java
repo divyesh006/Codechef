211f48c1e2a7a19ffee6a0194cc590ee005f39d7
divyesh006 <divyesh10008@gmail.com>
2022-07-28 17:36:48 +0530

    Codechef

---
+++ Counting Pretty Numbers/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0){
+            int l = sc.nextInt();
+            int r = sc.nextInt();
+            int count = 0;
+            for (int i = l;i <= r;i++){
+                if (i % 10 == 2 || i % 10 == 3 || i % 10 == 9){
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
